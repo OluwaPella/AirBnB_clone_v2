df1958c00cb6c61175cb07f40505ab6e3e78df8a
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-11-16 02:18:50 +0100

    add: public method to the exiting

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,6 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+        def close(self):
+            """ this method deserializing the JSON file to objects"""
+            self.reload()
